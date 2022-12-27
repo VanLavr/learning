f6c2daee97749932682070778a079a95a250f08d
Ivan <lavr.i-n@mail.ru>
2022-12-27 21:37:05 +0300

    generics added...

--- interfacesAndGenerics.go
+++ interfacesAndGenerics.go
@@ -2,6 +2,10 @@ package main
 
 import "fmt"
 
+type AnyNumber interface {
+	int | int8 | int16 | int32 | float32 | float64
+}
+
 type Greeter interface {
 	greet(string)
 }
@@ -21,6 +25,10 @@ func SayHi(g Greeter, name string) {
 	g.greet(name)
 }
 
+func Summation[T AnyNumber](a T, b T) T {
+	return a + b
+}
+
 func main() {
 	var (
 		GoodBoy = "Ivan"
@@ -31,4 +39,8 @@ func main() {
 
 	SayHi(&B, BadBoy)
 	SayHi(&G, GoodBoy)
+
+	res1 := Summation(1, 3)
+	res2 := Summation(1.1, 3.3)
+	fmt.Println(res1, res2)
 }

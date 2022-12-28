b339727118ca0085b82cd8cb00209ad12c07043a
Ivan <lavr.i-n@mail.ru>
2022-12-28 14:09:59 +0300

    added more interfaces (logics from habr)

--- interfacesAndGenerics.go
+++ interfacesAndGenerics.go
@@ -1,10 +1,57 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
 type AnyNumber interface {
 	int | int8 | int16 | int32 | float32 | float64
 }
+
+func Summation[T AnyNumber](a T, b T) T {
+	return a + b
+}
+
+
+
+type NumCollector interface {
+	NumRet() int
+}
+
+type ElementOfArray struct {
+	value string
+	index int
+}
+
+type Element struct {
+	value string
+}
+
+func (e *ElementOfArray) NumRet() int {
+	val, err := strconv.Atoi(e.value)
+	if err != nil {
+		panic(err)
+	}
+	val += e.index
+
+	return val
+}
+
+func (e *Element) NumRet() int {
+	val, err := strconv.Atoi(e.value)
+	if err != nil {
+		panic(err)
+	}
+
+	return val
+}
+
+func AnyFunction(n NumCollector) {
+	fmt.Println(n.NumRet())
+}
+
+
 
 type Greeter interface {
 	greet(string)
@@ -25,9 +72,7 @@ func SayHi(g Greeter, name string) {
 	g.greet(name)
 }
 
-func Summation[T AnyNumber](a T, b T) T {
-	return a + b
-}
+
 
 func main() {
 	var (
@@ -43,4 +88,12 @@ func main() {
 	res1 := Summation(1, 3)
 	res2 := Summation(1.1, 3.3)
 	fmt.Println(res1, res2)
+
+
+
+	var el1 = Element{value: "16"}
+	var el2 = ElementOfArray{value: "16", index: 11}
+
+	AnyFunction(&el1)
+	AnyFunction(&el2)
 }

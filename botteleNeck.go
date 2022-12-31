e2a36a65111d1e6d74c18e1d7e800ef1079203d2
Ivan <lavr.i-n@mail.ru>
2022-12-31 15:29:01 +0300

    learned a select statement...

---
+++ botteleNeck.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	out1 := make(chan string)
+	out2 := make(chan string)
+
+	go func() {
+		for {
+			time.Sleep(time.Millisecond * 500)
+			out1 <- "A: doing pullup"
+		}
+	}()
+
+	go func() {
+		for {
+			time.Sleep(time.Millisecond * 2500)
+			out1 <- "BB: doing cluster"
+		}
+	}()
+
+	for {
+		select {
+		case messege := <- out1:
+			fmt.Println(messege)
+
+		case messege := <- out2:
+			fmt.Println(messege)
+		}
+	}
+}

648050b9b1ce697c6d9f2d33dd3ad8984c5c7809
Ivan <lavr.i-n@mail.ru>
2023-02-04 00:09:42 +0300

    doing animation (onigiri youtube)

--- afterLongPause/animation/main.go
+++ afterLongPause/animation/main.go
@@ -1,3 +1,6 @@
+
+// screen[i * width + j] = pixel // filling full screen
+
 package main
 
 import (
@@ -11,22 +14,30 @@ const screenLenght int = width * height
 var screen [screenLenght]string
 
 var pixel string = " "
+var borderRelation float64 = float64(width) / float64(height) // relation width to height ( 4 : 1 )
+var symbolFrameRelation float64 = 11.0 / 24.0
+// var symbolFrameRelation float64 = 8.0 / 16.0
 
 
 func main() {
 	
-	for i := 0; i < width; i++ {
-		for j := 0; j < height; j++ {
+	for i := 0; i < height; i++ {
+		for j := 0; j < width; j++ {
+			pixel = " "
 			
-			var x float64 = 1 * ( float64(i) / float64(width) )
-			var y float64 = 1 * ( float64(j) / float64(height) )
+			var x float64 = 2 * ( ( float64(j) / float64(width) ) - 0.5 ) // normalizing width (from -1 to 1)
+			var y float64 = 2 * ( ( float64(i) / float64(height) ) - 0.5 ) // normalizing height (from -1 to 1)
+			x *= borderRelation * symbolFrameRelation // x and y coordinates in consider with screen and symbol border relations
 
-			if ( ((x * x) + (y * y)) < 0.5 ) {
+
+			if ( (x * x + y * y) < 0.5 ) {
 				pixel = "@"
 			}
-			screen[i + j * width] = pixel
+			screen[i * width + j] = pixel
+			//fmt.Println(x, y)
 		}
 	}
+
 	PrintScreen(screen)
 
 }
